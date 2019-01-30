b768801652158008831e9553a72f98602c9960ac
iSantonica <anaphemaa@gmail.com>
2019-01-30 15:07:11 +0200

    all from stakhow

--- src/js/common.js
+++ src/js/common.js
@@ -19,9 +19,65 @@ $('#show-cart').on('click', function(){
   }
 });
 
-  $('.scrollbar-inner').scrollbar(); 
+  $('.scrollbar-inner').scrollbar();
 
-});
+  $('.amanit-product__slider .slider-wrap').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    dotsClass: 'amanit-dots'
+  });
+
+  /*  dima search-form   */
+  $('.search-input .form-input').on('input',function () {
+    if($(this).val().length > 0){
+      $(this).parent().find('.clear-input').css('display', 'inline-block');
+    } else {
+      $(this).parent().find('.clear-input').css('display', 'none');
+    }
+    $(this).parent().find('.clear-input').click(function(){
+      $(this).css('display', 'none');
+    });
+  }); // end on
+
+  /*
+  * dima: Сопутствующте услуги:
+  * */
+
+  $('.amanit-product-card_related-services .js-link').click(function(){
+    let jsLinlCurText = $(this).attr('data-more');
+    let jsLinlNextText = $(this).attr('data-less');
+    $(this)
+      .toggleClass('active')
+      .text($(this).hasClass('active') ? jsLinlNextText : jsLinlCurText)
+      .closest('.amanit-product-card_related-services')
+      .find('.related-services__bot').slideToggle();
+  }); // end click
+  $('.toggle-box .js-link').click(function(){
+    let jsLinlCurText = $(this).attr('data-more');
+    let jsLinlNextText = $(this).attr('data-less');
+    $(this)
+      .toggleClass('active')
+      .text($(this).hasClass('active') ? jsLinlNextText : jsLinlCurText)
+      .parent('.toggle-box')
+      .find('.toggle-box__inner')
+      .toggleClass('active');
+    console.log('click');
+  });
+  $('.toggle-box .js-toogle-link ').click(function(){
+    let jsLinlCurText = $(this).attr('data-more');
+    let jsLinlNextText = $(this).attr('data-less');
+    $(this)
+      .text($(this).hasClass('active') ? jsLinlNextText : jsLinlCurText)
+      .parent('.toggle-box')
+      .find('.toggle-box__inner')
+      .toggleClass('active');
+    console.log('click');
+  }); 
+
+});//end ready
 
 
 })(jQuery);
